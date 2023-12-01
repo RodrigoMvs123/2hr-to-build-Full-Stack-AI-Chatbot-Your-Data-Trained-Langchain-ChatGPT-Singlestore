ab8c6532540aafc1eaabfc5b250adca1e4f5cfb8
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2023-12-01 09:01:11 -0300

    Create LogOutIcon.tsx

---
+++ LogOutIcon.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const LogOutIcon = () => {
+    return (
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- License: MIT. Made by Software Mansion: https://github.com/kamilagraf/react-swm-icon-pack -->
+        <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H15M8 8L4 12M4 12L8 16M4 12L16 12" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+export default LogOutIcon
